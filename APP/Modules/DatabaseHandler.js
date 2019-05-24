5f1128c6dc0afb6fbe030ed12b902b89f9240cbc
Stanley York <stanley.s.york@gmail.com>
2019-05-24 12:45:35 +0100

    Added MongoDB as a dependancy

--- APP/Modules/DatabaseHandler.js
+++ APP/Modules/DatabaseHandler.js
@@ -1,4 +1,6 @@
 /**
- * This module is for managing all SQL calls to the Database hosted in Azure
- * Database address: foxbot.database.windows.net
+ * This module will handle all calls to the MongoDB Database that will run on
+ * ARES. This will be a locally hosted Database.
  */
+
+ 
